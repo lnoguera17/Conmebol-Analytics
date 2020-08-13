dd150dadc2b66a5829cb87b6ca318f52cc0dd76d
Nicolas Kaswalder <nicokaswalder@gmail.com>
2020-08-12 20:14:52 -0400

    added position to the data cleaning script

--- Data Cleaning Script.R
+++ Data Cleaning Script.R
@@ -9,12 +9,18 @@ Conmebol_raw <- read_csv("conmebol.csv") %>%
 
 #Lets convert value into a number and clean the names of the players
 
-Conmebol <- Conmebol_raw %>%
+Conmebol_num <- Conmebol_raw %>%
   mutate(value2 = str_sub(value,-1,-1),
          value = parse_number(value),
          value = case_when(value2 == "M" ~ value * 1000000,
                            value2 == "K" ~ value * 1000,
-                           TRUE ~ 0)) %>% 
+                           TRUE ~ 0),
+         position = case_when(best_position %in% c("ST","RW","LW","CF") ~ "ATT",
+                              best_position %in% c("RB","RWB","LWB","CB","LB") ~ "DEF",
+                              best_position == "GK" ~ "GK",
+                              TRUE ~ "MID"))
+
+Conmebol <- Conmebol_num %>% 
   na_if(.,0) %>% 
   separate(height, c("Foot", "Inches")) %>%
   mutate(height = as.double(str_c(Foot, '.', Inches))) %>% 
